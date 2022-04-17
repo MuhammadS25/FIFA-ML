e8280e89139d9bf18510904f0739ca1c0c7cf3fb
Mazen Mohamed Bakr <71599149+IX0XI@users.noreply.github.com>
2022-04-18 00:49:56 +0200

    Preprocessing
    dropping data

--- Model.py
+++ Model.py
@@ -5,25 +5,45 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OrdinalEncoder
+import re
 
 #Load players data
 data = pd.read_csv('player-value-prediction.csv')
 #checking if data read correctly
-print(data.head())
+#print(data.head())
 #Checking for missing values
-print(data.isna().sum())
+#print(data.isna().sum())
 #Pre-processing
 
 ##Converting Date String to Date to apply Processes on it
 #data['birth_date']=pd.to_datetime(data['birth_date'])
-##Changing Date format to days which could be represented as one meaningful number
+##Changing Date format to days wh(ich could be represented as one meaningful number
 #now = datetime.now()
 #data['birth_date']=now - data['birth_date'] #subtracting current date from it to get age in days
 
 ##splitting position column into separate columns
 
+
+
+data[['w','x','y','z']]= data['positions'].str.split(',',expand=True)
+data['w'].fillna(0 ,inplace=True)
+data['x'].fillna(0 ,inplace=True)
+data['y'].fillna(0 ,inplace=True)
+data['z'].fillna(0 ,inplace=True)
+data.loc[data['w'] != 0,'w']=1
+data.loc[data['x'] != 0,'x']=1
+data.loc[data['y'] != 0,'y']=1
+data.loc[data['z'] != 0,'z']=1
+data['positions']=data['w']+data['x']+data['y']+data['z']
+data.drop(columns=['w','x','y','z'],inplace=True,axis=1)
+data.drop(columns=['national_team','national_rating','national_team_position','national_jersey_number'],inplace=True,axis=1)
+data.dropna(inplace=True,axis=0)
+print(data.isna().any())
 #Features
+
 X=data.iloc[:,4:91]
-print(X)
+print(X.head())
+print(X['positions'])
 #label
 Y=data['value']
